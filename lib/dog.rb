31711f2b73686ba96142094ac5b138aa161f9c75
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-07-03 05:58:17 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,35 @@
+class Dog 
+  def initialize(name)
+    @name = name
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+end
+
+  
+  fido = Dog.new 
+  fido.name = "Fido"
+  
+  puts fido.name
+  
+  class Dog
+    def initialize(breed)
+      @breed = breed
+    end
+    
+    def breed
+      @breed
+    end
+  end
+  
+  fido = Dog.new
+  fido.breed
+  fido.breed = "Mutt"
+    
+  
